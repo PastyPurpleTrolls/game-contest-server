b178da39a11b945051aae5b78266f3f87aca1c99
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-01-09 15:20:34 -0500

    Fixed broken rounds.show

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,6 +1,6 @@
 class RoundsController < ApplicationController
   before_action :ensure_user_logged_in, only: [:show]
 	def show
-		@show = Round.find(params[:id])
+		@show = Round.friendly.find(params[:id])
 	end
 end
