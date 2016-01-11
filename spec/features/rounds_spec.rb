383bfeda567413e2ebd2c94ef219dd4d6f80ea81
Mike Kammes <mkammes@game.(none)>
2016-01-11 14:50:41 -0500

    Made changes for Zach to pass roundsPages tests

--- spec/features/rounds_spec.rb
+++ spec/features/rounds_spec.rb
@@ -7,12 +7,12 @@ describe "RoundsPages" do
     let (:tournament_match) { FactoryGirl.create(:tournament_match, manager: tournament) }
     let (:tournament_match_2) { FactoryGirl.create(:tournament_match, manager: tournament) }
     let (:contest) { FactoryGirl.create(:contest) }
-		let (:contest_match) { FactoryGirl.create(:contest_match, manager: contest) }
+		let (:challenge_match) { FactoryGirl.create(:challenge_match, manager: contest) }
 
     before do
       5.times { FactoryGirl.create(:round, match: tournament_match) }
       5.times { FactoryGirl.create(:round, match: tournament_match_2) }
-      5.times { FactoryGirl.create(:round, match: contest_match) }
+      5.times { FactoryGirl.create(:round, match: challenge_match) }
 			visit match_rounds_path(:tournament_match)
 		end
 
@@ -28,7 +28,7 @@ describe "RoundsPages" do
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
-			Rounds.where(match: contest_match).each do |r|
+			Rounds.where(match: challenge_match).each do |r|
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
@@ -39,25 +39,25 @@ describe "RoundsPages" do
     let (:tournament) { FactoryGirl.create(:tournament) }
     let (:tournament_match) { FactoryGirl.create(:tournament_match, manager: tournament) }
     let (:contest) { FactoryGirl.create(:contest) }
-		let (:contest_match) { FactoryGirl.create(:contest_match, manager: contest) }
-		let (:contest_match_2) { FactoryGirl.create(:contest_match, manager: contest) }
+		let (:challenge_match) { FactoryGirl.create(:challenge_match, manager: contest) }
+		let (:challenge_match_2) { FactoryGirl.create(:challenge_match, manager: contest) }
 
     before do
       5.times { FactoryGirl.create(:round, match: tournament_match) }
-      5.times { FactoryGirl.create(:round, match: contest_match) }
-      5.times { FactoryGirl.create(:round, match: contest_match_2) }
-			visit match_rounds_path(:contest_match)
+      5.times { FactoryGirl.create(:round, match: challenge_match) }
+      5.times { FactoryGirl.create(:round, match: challenge_match_2) }
+			visit match_rounds_path(:challenge_match)
 		end
 
 		it "lists all the rounds for the match" do
-			Rounds.where(match: contest_match).each do |r|
+			Rounds.where(match: challenge_match).each do |r|
 				should have_selector('li', text: r.id)	
         should have_link(r.id, round_path(r))
 			end
 		end
 
 		it "should not list rounds of a different match" do
-			Rounds.where(match: contest_match_2).each do |r|
+			Rounds.where(match: challenge_match_2).each do |r|
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
