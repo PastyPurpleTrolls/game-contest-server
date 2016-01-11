5c89ecc5d52b6a7a2c616a2523c19995eef5e753
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-01-11 15:32:50 -0500

    Created test referee

---
+++ examples/test_ref.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+
+import json
+from ref_helper import *
+
+class Player():
+    def __init__(self, server):
+        self.wins = 0
+
+        self.connection = Connection(server)
+        self.name = self.connection.listen(1024)
+
+    def addWin(self):
+        self.wins += 1
+        return self.wins
+
+    def move(self):
+        self.connection.send('move')
+        move = self.connection.listen(1024)
+        return move
+
+class Game():
+
+    def __init__(self, numPlayers):
+        self.players = []
+
+        #Create players
+        for i in range(numPlayers):
+            self.players.append(Player(playerServer))
+
+        manager.send("match", "start")
+
+        winner = self.runGame()
+        manager.send("round", "end")
+        self.reportResults("roundresult", winner)
+
+        manager.send("match", "end")
+        self.reportResults("matchresult", winner)
+
+    def reportResults(self, resulttype, winner):
+        for player in self.players:
+            #Tell the player who won
+            player.connection.send(winner.name + " wins\n")
+            #Get the result for who won
+            result = "Win" if (player.name == winner.name) else "Loss"
+            manager.send(resulttype, [player.name, result, str(player.wins)])
+
+    def runGame(self):
+        #Loop until someone wins
+        manager.send("round", ["start", 0])
+        while True:
+            for player in self.players:
+                move = player.move()
+                #Report move to manager
+                manager.send("move", [player.name + " plays " + move, json.dumps([player.name, move])])
+                #Check if it's a winning move
+                if self.checkWin(move):
+                    player.addWin()
+                    return player
+
+    def checkWin(self, move):
+        return move == 'w'
+
+game = Game(options.num)
--- exec_environment/wrapper_test.rb
+++ exec_environment/wrapper_test.rb
@@ -16,18 +16,14 @@ class MockPlayer
     end
 end
 
-p1 = MockPlayer.new("../examples/checkers_player.py","first")
-p2 = MockPlayer.new("../examples/checkers_player.py", "second")
-ref = MockPlayer.new("../examples/checkers_ref.py", "ref")
-
-
-#p1 = MockPlayer.new("../spec/exec_environment/test_player.py","first")
-#p2 = MockPlayer.new("../spec/exec_environment/test_player.rb", "second")
-#ref = MockPlayer.new("../spec/exec_environment/test_referee.rb", "ref")
+p1 = MockPlayer.new("../examples/test_player.py","first")
+p2 = MockPlayer.new("../examples/test_player.py", "second")
+ref = MockPlayer.new("../examples/test_ref.py", "ref")
 
 round_wrapper = RoundWrapper.new(ref, 2, 20, [p1, p2], 3)
 round_wrapper.run_match
 
 #puts(round_wrapper.status)
+puts round_wrapper.rounds
 
 
