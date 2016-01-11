a12ba65c78cb9b6919ea063fd29bd2f0f3913d85
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-01-11 15:43:09 -0500

    Fixed wrapper tester

--- exec_environment/wrapper_test.rb
+++ exec_environment/wrapper_test.rb
@@ -1,8 +1,6 @@
 #! /usr/bin/env ruby
 #
-#Alex Sjoberg
 #wrapper_test.rb
-#Jan 2014
 #Can be used test any players and referees without having to worry about tournaments/databases/etc
 
 require_relative 'round_wrapper'
@@ -20,7 +18,7 @@ p1 = MockPlayer.new("../examples/test_player.py","first")
 p2 = MockPlayer.new("../examples/test_player.py", "second")
 ref = MockPlayer.new("../examples/test_ref.py", "ref")
 
-round_wrapper = RoundWrapper.new(ref, 2, 20, [p1, p2], 3)
+round_wrapper = RoundWrapper.new(ref, 2, 5, [p1, p2], 3)
 round_wrapper.run_match
 
 #puts(round_wrapper.status)
