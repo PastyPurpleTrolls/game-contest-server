51c8055cc716cf297c5f12fcfb240cff35903e1e
John Mathews <john_mathews@taylor.edu>
2016-01-11 11:04:26 -0500

    also changed player file to .py instead of .rb

--- spec/exec_environment/match_wrapper_spec.rb
+++ spec/exec_environment/match_wrapper_spec.rb
@@ -45,8 +45,8 @@ describe "MatchWrapper" do
   before :each do
     @user = FactoryGirl.create(:user)
     @contest = FactoryGirl.create(:contest)
-    @player1 = FactoryGirl.create(:player, user: @user, contest: @contest, name: 'dumb_player', file_location: Rails.root.join('spec', 'exec_environment', '../files/dumb_player.rb').to_s )
-    @player2 = FactoryGirl.create(:player, user: @user, contest: @contest, name: 'stupid_player', file_location: Rails.root.join('spec', 'exec_environment', '../files/dumb_player.rb').to_s )
+    @player1 = FactoryGirl.create(:player, user: @user, contest: @contest, name: 'dumb_player', file_location: Rails.root.join('spec', 'exec_environment', '../files/dumb_player.py').to_s )
+    @player2 = FactoryGirl.create(:player, user: @user, contest: @contest, name: 'stupid_player', file_location: Rails.root.join('spec', 'exec_environment', '../files/dumb_player.py').to_s )
     @referee = FactoryGirl.create(:player, name: "referee", file_location: Rails.root.join('spec', 'exec_environment', '../../examples/test_referee.rb').to_s )
     @match_wrapper = MatchWrapper.new(@referee , 2, 5, [@player1, @player2], 1)
   end
