3c5e91c5895ecd7fbd903d6eb3033652ec4a5148
Zach Alford <zalford@game.(none)>
2016-01-12 11:16:11 -0500

    Now passing 2 tests in spec/features/authorization_pages_spec.rb related to Rounds' Show method

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,10 +2,8 @@ class RoundsController < ApplicationController
   before_action :ensure_user_logged_in, only: [:show]
 	def show
 		@round = Round.friendly.find(params[:id])
-		unless @round.match.tournament_match?
-			# ensure that user is logged in, and that the user has a player in the challenge match
-			@list_of_users_in_match = list_of_users_in_match(@round.match)
-			ensure_correct_user_from_list(@list_of_users_in_match, 'You do not have a player in this round')
-		end
+		# ensure that user is logged in, and that the user has a player in the challenge match
+		@list_of_users_in_match = list_of_users_in_match(@round.match)
+		ensure_correct_user_from_list(@list_of_users_in_match, 'You do not have a player in this round')
 	end
 end
