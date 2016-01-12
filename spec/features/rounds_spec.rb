5516e14c0068af6703dd6a5f3c67a971b52a9883
Jonathan Geisler <jgeisler@cse.taylor.edu>
2016-01-12 00:34:05 -0500

    Refactor round tests
    Fix up the round tests so they should be more correct.

--- spec/features/rounds_spec.rb
+++ spec/features/rounds_spec.rb
@@ -10,25 +10,25 @@ describe "RoundsPages" do
 		let (:contest_match) { FactoryGirl.create(:contest_match, manager: contest) }
 
     before do
-      5.times { FactoryGirl.create(:round, match: tournament_match) }
-      5.times { FactoryGirl.create(:round, match: tournament_match_2) }
-      5.times { FactoryGirl.create(:round, match: contest_match) }
-			visit match_rounds_path(:tournament_match)
+			FactoryGirl.create_list(:round, 5, match: tournament_match)
+			FactoryGirl.create_list(:round, 5, match: tournament_match_2)
+			FactoryGirl.create_list(:round, 5, match: contest_match)
+			visit match_rounds_path(tournament_match)
 		end
 
 		it "lists all the rounds for the match" do
-			Rounds.where(match: tournament_match).each do |r|
+			Round.where(match: tournament_match).each do |r|
 				should have_selector('li', text: r.id)	
         should have_link(r.id, round_path(r))
 			end
 		end
 
 		it "should not list rounds of a different match" do
-			Rounds.where(match: tournament_match_2).each do |r|
+			Round.where(match: tournament_match_2).each do |r|
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
-			Rounds.where(match: contest_match).each do |r|
+			Round.where(match: contest_match).each do |r|
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
@@ -43,25 +43,25 @@ describe "RoundsPages" do
 		let (:contest_match_2) { FactoryGirl.create(:contest_match, manager: contest) }
 
     before do
-      5.times { FactoryGirl.create(:round, match: tournament_match) }
-      5.times { FactoryGirl.create(:round, match: contest_match) }
-      5.times { FactoryGirl.create(:round, match: contest_match_2) }
-			visit match_rounds_path(:contest_match)
+			FactoryGirl.create_list(:round, 5, match: tournament_match)
+			FactoryGirl.create_list(:round, 5, match: tournament_match_2)
+			FactoryGirl.create_list(:round, 5, match: contest_match_2)
+			visit match_rounds_path(contest_match)
 		end
 
 		it "lists all the rounds for the match" do
-			Rounds.where(match: contest_match).each do |r|
+			Round.where(match: contest_match).each do |r|
 				should have_selector('li', text: r.id)	
         should have_link(r.id, round_path(r))
 			end
 		end
 
 		it "should not list rounds of a different match" do
-			Rounds.where(match: contest_match_2).each do |r|
+			Round.where(match: contest_match_2).each do |r|
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
-			Rounds.where(match: tournament_match).each do |r|
+			Round.where(match: tournament_match).each do |r|
 				should_not have_selector('li', text: r.id)	
         should_not have_link(r.id, round_path(r))
 			end
