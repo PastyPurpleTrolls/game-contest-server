474c1e0a1d5bf8e0a43e4a3bf79d06956c889358
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-01-12 12:33:44 -0500

    Allow professors to upload replay assets with referees

--- app/models/concerns/uploadable.rb
+++ app/models/concerns/uploadable.rb
@@ -1,75 +1,89 @@
+require 'fileutils'
+
 module Uploadable
-  extend ActiveSupport::Concern
+    extend ActiveSupport::Concern
 
-  included do
-    before_destroy :delete_code_locations
-    validate :file_location_exists
-  end
+    included do
+        before_destroy :delete_code_locations
+        validate :file_location_exists
+    end
 
-def upload=(uploaded_io)
-    unless self.name.blank?
-      random_hex = SecureRandom.hex
-      self.file_location = '' if self.file_location.nil?
-      delete_code(self.file_location)
-      self.file_location = store_file(uploaded_io, self.class.to_s.downcase.pluralize, random_hex)
-      uncompress(self.contest.referee.compressed_file_location, File.dirname(self.file_location)) if self.class == Player 
+    def upload=(uploaded_io)
+        unless self.name.blank?
+            random_hex = SecureRandom.hex
+            self.file_location = '' if self.file_location.nil?
+            delete_code(self.file_location)
+            self.file_location = store_file(uploaded_io, self.class.to_s.downcase.pluralize, random_hex)[:file]
+            uncompress(self.contest.referee.compressed_file_location, File.dirname(self.file_location)) if self.class == Player 
+        end
+    end
+
+    def upload2=(uploaded_io)
+        unless self.name.blank?
+            random_hex = SecureRandom.hex
+            self.compressed_file_location = '' if self.compressed_file_location.nil?
+            delete_code(self.compressed_file_location)
+            self.compressed_file_location = store_file(uploaded_io, 'environments', random_hex)[:file]
+        end
+    end
+
+    def upload3=(uploaded_io)
+        unless self.name.blank?
+            random_hex = SecureRandom.hex
+            self.replay_assets_location = '' if self.replay_assets_location.nil?
+            delete_code(self.replay_assets_location)
+            self.replay_assets_location = store_file(uploaded_io, File.join("static", self.class.to_s.downcase.pluralize), random_hex)[:directory]
+        end
+    end
+
+
+    #def compressed_file_location_exists
+    #   if self.compressed_file_location.nil? || !File.exists?(self.compressed_file_location)
+    #	errors.add(:compressed_file_location, "doesn't exist on the server")
+    #   end
+    #end 
+    def file_location_exists
+        if self.file_location.nil? || !File.exists?(self.file_location)
+            errors.add(:file_location, "doesn't exist on the server")
+        end
+    end
+
+    private
+
+    def delete_code_locations
+        delete_code(self.file_location)
+        delete_code(self.compressed_file_location) if self.compressed_file_location?
+        delete_code(self.replay_assets_location) if self.replay_assets_location?
+    end
+
+    #Delete directory where file is located
+    def delete_code(location)
+        pathname = Pathname.new(location)
+        if not pathname.directory?
+            location = pathname.dirname
+        end
+        FileUtils.rm_rf(location) if pathname.exist?
+    end
+
+    def store_file(uploaded_io, dir, random_hex)
+        file_location = ''
+        dir_location = ''
+        unless uploaded_io.nil?
+            dir_location = Rails.root.join('code', dir, Rails.env, random_hex)
+            file_location = dir_location.join(self.name).to_s
+            dir_location = dir_location.to_s
+            FileUtils.mkdir_p dir_location
+            IO.copy_stream(uploaded_io, file_location)
+            uncompress(file_location, dir_location)
+        end
+        {file: file_location, directory: dir_location}
+    end
+
+    def uncompress(src, dest)
+        system("tar -xvf #{src} -C #{dest} > /dev/null 2>&1") 
+        system("unzip #{src} -d #{dest} > /dev/null 2>&1")
+        system("chmod +x #{dest}/*")
+        system("dos2unix -q #{dest}/*")
     end
 end
-
-def upload2=(uploaded_io)
-    unless self.name.blank?
-      random_hex = SecureRandom.hex
-      self.compressed_file_location = '' if self.compressed_file_location.nil?
-      delete_code(self.compressed_file_location)
-      self.compressed_file_location = store_file(uploaded_io, 'environments', random_hex)
-    end
-end
-
-#def compressed_file_location_exists
-#   if self.compressed_file_location.nil? || !File.exists?(self.compressed_file_location)
-#	errors.add(:compressed_file_location, "doesn't exist on the server")
-#   end
-#end 
- def file_location_exists
-   if self.file_location.nil? || !File.exists?(self.file_location)
-      errors.add(:file_location, "doesn't exist on the server")
-   end
- end
-
-  private
-
-  def delete_code_locations
-    delete_code(self.file_location)
-    delete_code(self.compressed_file_location) if self.attributes.include? :compressed_file_location
-  end
-
-  def delete_code(location)
-    File.delete(location) if File.exists?(location)
-  end
-
-  def store_file(uploaded_io, dir, random_hex)
-    file_location = ''
-    unless uploaded_io.nil?
-      dir_location = Rails.root.join('code',
-                                      dir,
-				      Rails.env,
-				      random_hex)
-      file_location = dir_location.join(self.name).to_s
-      dir_location = dir_location.to_s
-      system("mkdir #{dir_location}")
-      IO.copy_stream(uploaded_io, file_location)
-      uncompress(file_location, dir_location)
-   end
-		
-  file_location
-  end
-
-  def uncompress(src, dest)
-    system("tar -xvf #{src} -C #{dest} > /dev/null 2>&1") 
-    system("unzip #{src} -d #{dest} > /dev/null 2>&1")
-    system("chmod +x #{dest}/*")
-    system("dos2unix -q #{dest}/*")
-  end
-
- end
 
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,34 +1,34 @@
 require 'uri'
 
 class Referee < ActiveRecord::Base
-  #  belongs_to :programming_language
-  belongs_to :user
-  has_many :contests
+    #  belongs_to :programming_language
+    belongs_to :user
+    has_many :contests
 
-  validates :user,              presence: true
-  validates :match_limit,       presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :name,              presence: true, uniqueness: true
-  validates :rules_url,		format: { with: /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(([\/][a-z0-9]*)*(([\/][a-z0-9]+([\-]{1}[a-z0-9]+)?)*\.[a-z]{2,6})?)?([\?]([a-z0-9]+[\=][a-z0-9]+[\&]?)*)?\z/ }
-  validates :players_per_game,  numericality: { only_integer: true, greater_than: 0, less_than: 11 }
-  validates :time_per_game,     numericality: { only_integer: true, greater_than: 0, less_than: 16 }
-  #  validates :programming_language, presence: true
+    validates :user,              presence: true
+    validates :match_limit,       presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :name,              presence: true, uniqueness: true
+    validates :rules_url,		format: { with: /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(([\/][a-z0-9]*)*(([\/][a-z0-9]+([\-]{1}[a-z0-9]+)?)*\.[a-z]{2,6})?)?([\?]([a-z0-9]+[\=][a-z0-9]+[\&]?)*)?\z/ }
+    validates :players_per_game,  numericality: { only_integer: true, greater_than: 0, less_than: 11 }
+    validates :time_per_game,     numericality: { only_integer: true, greater_than: 0, less_than: 16 }
+    #  validates :programming_language, presence: true
 
-  include Uploadable
+    include Uploadable
 
 
-  def self.search(search)
-    if search
-      where('name LIKE ?', "%#{search}%")
-    else
-      all
+    def self.search(search)
+        if search
+            where('name LIKE ?', "%#{search}%")
+        else
+            all
+        end
     end
-  end
 
-  extend FriendlyId
-  friendly_id :name, use: :slugged
-  after_validation :move_friendly_id_error_to_name
+    extend FriendlyId
+    friendly_id :name, use: :slugged
+    after_validation :move_friendly_id_error_to_name
 
-  def move_friendly_id_error_to_name
-    errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
-  end
+    def move_friendly_id_error_to_name
+        errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+    end
 end
