5ee62334df74e97a0d0f3f0afc05625ef1b1ee15
John Mathews <john_mathews@taylor.edu>
2016-01-13 17:26:43 -0500

    working on player controller tests

--- app/models/concerns/uploadable.rb
+++ app/models/concerns/uploadable.rb
@@ -52,8 +52,8 @@ module Uploadable
 
     def delete_code_locations
         delete_code(self.file_location)
-        delete_code(self.compressed_file_location) if self.compressed_file_location
-        delete_code(self.replay_assets_location) if self.replay_assets_location
+        delete_code(self.compressed_file_location) if self.has_attribute?(:compressed_file_location)
+        delete_code(self.replay_assets_location) if self.has_attribute?(:replay_assets_location)
     end
 
     #Delete directory where file is located
