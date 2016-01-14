3ac7ca69d4fc6197d4d38a679cbc2f9d7b3ea47a
John Mathews <john_mathews@taylor.edu>
2016-01-14 13:31:41 -0500

    so I can pull

--- app/models/player_round.rb
+++ app/models/player_round.rb
@@ -2,8 +2,10 @@ class PlayerRound < ActiveRecord::Base
   belongs_to :round, inverse_of: :player_rounds
   belongs_to :player
 
-	validates :round_id, :presence => true
-	validates :player_id, :presence => true
+	validates :player, presence: true
+	validates :round, presence: true
+#	validates :round_id, :presence => true
+#	validates :player_id, :presence => true
   validates :result,  	inclusion: [nil, 'Win', 'Loss', 'Tie', 'Crash', 'Time out', 'Unknown Round Result']
 	
 	validate :check_ids
