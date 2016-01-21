4b861d8ff10f6c092c3a40dc26525ecb5a3033fb
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-01-21 10:00:16 -0500

    Removed index.md files in favor of category named help files

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -5,7 +5,8 @@ class HelpController < ApplicationController
     def show
         @category = params[:category]
         #Make sure the user is accessing a "page" and remove all non alphanumeric characters
-        @page = (params[:page] || "index").downcase.gsub(/[^a-z0-9]/, '')
+        #If the user does not provide a page, look for a file with the same name as the category
+        @page = (params[:page] || @category).downcase.gsub(/[^a-z0-9]/, '')
         filename = File.join(Rails.root, 'documentation', @category, @page + '.md')
         #Catch errors (usually if the file doesn't exist)
         begin 
