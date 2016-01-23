ffc0bc499959994809abff5261a84be1b60200bc
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-01-23 09:51:34 -0500

    Added new flags in make file example

--- examples/Makefile.py.mk
+++ examples/Makefile.py.mk
@@ -6,7 +6,7 @@ manager:
 	nc -l -p $(port)
 
 run: test_referee.rb
-	./test_referee.py -p $(port) -n $(num_players) $(num_matches) $(timeout)
+	./test_referee.py -p $(port) -n $(num_players) -r $(num_matches) -t $(max_time)
 
 contest: $(PLAYER)
 	./$(PLAYER).py -p $(port) -n '$(name)'
