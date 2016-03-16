227980048763bc876d2deee58e96932fdf63d859
Jonathan Geisler <jgeisler@cse.taylor.edu>
2016-03-16 00:48:35 -0400

    Attempt at better URL validation
    Based on
    http://stackoverflow.com/questions/7167895/whats-a-good-way-to-validate-links-urls-in-rails,
    this code should work, but testing it in IRB doesn't seem to do what I
    think it should.  Keeping it commented out until I can figure out what
    is wrong, but keeping it in so that it isn't lost forever.

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -9,6 +9,7 @@ class Referee < ActiveRecord::Base
   	validates :round_limit,       presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :name,              presence: true, uniqueness: true
     validates :rules_url,		format: { with: /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(([\/][a-z0-9]*)*(([\/][a-z0-9]+([\-]{1}[a-z0-9]+)?)*\.[a-z]{2,6})?)?([\?]([a-z0-9]+[\=][a-z0-9]+[\&]?)*)?\z/ }
+#    validate :valid_url
     validates :players_per_game,  numericality: { only_integer: true, greater_than: 0, less_than: 11 }
     validates :time_per_game,     numericality: { only_integer: true, greater_than: 0, less_than: 16 }
     validates :file_location,              presence: true
@@ -32,5 +33,18 @@ class Referee < ActiveRecord::Base
 
     def move_friendly_id_error_to_name
         errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+    end
+
+    private
+
+    def valid_url
+	unless self.rules_url.blank?
+	    uri = URI.parse(self.rules_url)
+	    if uri.kind_of?(URI::HTTP) || uri.scheme.nil?
+		self.errors.add(:rules_url, "is invalid")
+	    end
+	rescue URI::InvalidURIError
+	    self.errors.add(:rules_url, "is invalid")
+	end
     end
 end
