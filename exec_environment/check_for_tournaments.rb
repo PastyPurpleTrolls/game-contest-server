9ef61676879554524785aaf1211e89dbba1ec1fa
Hannes Widrig <hanneswidrig@gmail.com>
2018-01-05 09:41:33 -0600

    Beautifed 'because some people don't know how to do that'

--- exec_environment/check_for_tournaments.rb
+++ exec_environment/check_for_tournaments.rb
@@ -7,10 +7,10 @@
 # Tournament runner will then create necessary Match objects in the db
 
 tournament = Tournament.where("start < ? and status = ?", Time.now.utc, "waiting").first
-if not tournament.nil? then
-    tournament.status = "started"
-    tournament.save
-    puts "Daemon spawning tournament #"+tournament.id.to_s
-    pid = Process.spawn("rails runner exec_environment/tournament_runner.rb -t #{tournament.id}")
-    Process.wait pid
+if not tournament.nil?
+  tournament.status = "started"
+  tournament.save
+  puts "Daemon spawning tournament #" + tournament.id.to_s
+  pid = Process.spawn("rails runner exec_environment/tournament_runner.rb -t #{tournament.id}")
+  Process.wait pid
 end
--- exec_environment/wrapper_test.rb
+++ exec_environment/wrapper_test.rb
@@ -6,12 +6,13 @@
 require_relative 'round_wrapper'
 
 class MockPlayer
-    attr_accessor :file_location , :name, :rounds_capable
-    def initialize(file_location,name)
-        self.file_location = file_location
-        self.name = name
-        @rounds_capable = false
-    end
+  attr_accessor :file_location, :name, :rounds_capable
+
+  def initialize(file_location, name)
+    self.file_location = file_location
+    self.name = name
+    @rounds_capable = false
+  end
 end
 
 #p1 = MockPlayer.new("../examples/test_player.py","first")
@@ -30,10 +31,8 @@ p4 = MockPlayer.new("../examples/risk/risk_player.py", "fourth")
 ref = MockPlayer.new("../examples/risk/risk_referee.py", "ref")
 =end
 
-
 round_wrapper = RoundWrapper.new(ref, 2, 10, [p1, p2], 1)
 round_wrapper.run_match
 
 puts(round_wrapper.status)
 puts round_wrapper.rounds
-
