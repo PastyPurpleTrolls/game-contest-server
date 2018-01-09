849b9c40661bf0d6315bc35307c4082990b2c294
John Luscombe <john_luscombe@taylor.edu>
2018-01-09 15:24:57 -0500

    Finished most breadcrumbs

--- spec/support/login.rb
+++ spec/support/login.rb
@@ -2,11 +2,17 @@ def login(user, options = {})
   if options[:avoid_capybara]
     post sessions_path, params: { username: user.username, password: user.password }
   else
+    puts 10
     visit login_path
+    puts 11
     fill_in 'Username', with: user.username
+    puts 12
     fill_in 'Password', with: user.password
+    puts 13
     click_button 'Log In'
+    puts 14
     page.find('.alert')
+    puts 15
   end
 end
 
