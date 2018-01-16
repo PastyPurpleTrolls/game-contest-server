d2c9354b2a6c1ec86fe6fa0e0dd1c8c2900ed405
Andrew Jackson <andrew.c.j1995@gmail.com>
2018-01-16 15:51:58 -0500

    Changed python version to 3.4 and ran pep8 formatter

--- examples/guess-w/test_player.py
+++ examples/guess-w/test_player.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3.4
 
 # test_player.py
 # Douglas Brown
@@ -7,13 +7,13 @@
 
 # Guess W! player written in python. Can interact properly with test_referee.rb
 
-#imports
+# imports
 from optparse import OptionParser
 import socket
 from random import choice
 
-#Parsing command line arguments
-#Usage: client.py --name [name] -p [port]"
+# Parsing command line arguments
+# Usage: client.py --name [name] -p [port]"
 parser = OptionParser()
 parser.add_option("-p", "--port", action="store", type="int", dest="port")
 parser.add_option("-n", "--name", action="store", type="string", dest="name")
@@ -24,19 +24,18 @@ PORT = options.port
 NAME = options.name
 
 
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = socket.gethostbyname(HOST)
-s.connect((ip,PORT)) #Connect to server
+s.connect((ip, PORT))  # Connect to server
 
 message = str(NAME + "\n")
 s.send(message.encode())
 
-#Now receive data
+# Now receive data
 while True:
     reply = s.recv(4096).decode()
     if "move" in reply:
-        guesses = ['a','b','c','w']
+        guesses = ['a', 'b', 'c', 'w']
         blah = choice(guesses) + "\n"
         s.send(blah.encode())
     elif "wins" in reply:
