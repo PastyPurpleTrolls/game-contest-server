621fe8192d5b43a2a507c2fbda62eaf4c6c85101
Hannes Widrig <hanneswidrig@gmail.com>
2018-01-17 09:34:15 -0500

    Clean up and bug fix of array indexing, ready!

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -1,19 +1,20 @@
 class BracketsController < ApplicationController
+  # Display Single Elimination Tournament Bracket Visualized
   def show
     tourney = Tournament.friendly.find(params[:id])
     match_ids = get_match_ids(tourney.id)
     player_ids = get_player_ids(tourney.id)
-    stage1 = gen_match_paths(match_ids, player_ids)
-    stage2 = gen_scores(stage1, match_ids, player_ids)
-    stage3 = gen_names(stage1, stage2, match_ids, player_ids)
-    render json: stage3.to_json
+    tournament_tree = generate_match_paths(match_ids, player_ids)
+    results = generate_scores(tournament_tree, match_ids, player_ids)
+    players = generate_players(tournament_tree, results, match_ids, player_ids)
+    render json: players.to_json
   end
 
   private
   def get_match_ids(tourney_id)
     matches = []
-    res = Match.where(manager_id: tourney_id).reorder(id: :asc)
-    res.each do |row|
+    query = Match.where(manager_id: tourney_id).reorder(id: :asc)
+    query.each do |row|
       matches.push(row.id)
     end
     return matches
@@ -21,8 +22,8 @@ class BracketsController < ApplicationController
 
   def get_player_ids(tourney_id)
     players = []
-    res = PlayerTournament.where(tournament_id: tourney_id).reorder(id: :asc)
-    res.each do |row|
+    query = PlayerTournament.where(tournament_id: tourney_id).reorder(id: :asc)
+    query.each do |row|
       players.push(row.player_id)
     end
     return players
@@ -30,29 +31,28 @@ class BracketsController < ApplicationController
 
   def get_names(player_ids)
     names = []
-    res = Player.select(:id, :name).where(id: player_ids).reorder(id: :asc)
-    res.each do |row|
+    query = Player.select(:id, :name).where(id: player_ids).reorder(id: :asc)
+    query.each do |row|
       names.insert(0, row.name)
     end
     return names
   end
 
-  def gen_match_paths(matches, players)
-    i = 1
-    matches_per_round = 2
+  # Build the match tree structure that represents the tournament
+  def generate_match_paths(matches, players)
+    i, matches_per_round = 1, 2
     round_depth = Math.log2(players.length).ceil
-    res = MatchPath.select(:parent_match_id, :child_match_id, :result)
+    query = MatchPath.select(:parent_match_id, :child_match_id, :result)
     .where(parent_match_id: matches).reorder(parent_match_id: :asc)
     
     match_paths = Array.new(round_depth, [])
     match_paths[0] = [matches.first]
 
     while (i < round_depth)
-      j = 0
-      k = 0
+      j, k = 0, 0
       match_paths[i] = Array.new(matches_per_round, nil)
       while (k < match_paths[i - 1].count)
-        results = res.where(child_match_id: match_paths[i - 1][k])
+        results = query.where(child_match_id: match_paths[i - 1][k])
         if (!results.blank?)
           results.each do |row|
             match_paths[i][j] = row.parent_match_id
@@ -71,32 +71,29 @@ class BracketsController < ApplicationController
     return match_paths
   end
 
-  def gen_scores(stage1, matches, players)
-    i = 0
-    x = 0
-    matches_per_round = 1
+  # Build the respective scoring for the matches in the tournament
+  def generate_scores(stage1, matches, players)
+    i, matches_per_round = 0, 1
     round_depth = Math.log2(players.length).ceil
-    res = PlayerMatch.select(:player_id, :match_id, :result)
+    query = PlayerMatch.select(:player_id, :match_id, :result)
     .where(match_id: matches).reorder(match_id: :asc)
     scores = Array.new(round_depth, [])
 
     while (i < round_depth)
-      j = 0
-      k = 0
+      j, k = 0, 0
       scores[i] = Array.new(matches_per_round, nil)
       while (k < scores[i].count)
-        results = res.where(match_id: stage1[i][k]).first
+        results = query.where(match_id: stage1[i][k]).first
         if (!results.blank?)
           if results['result'] == "Win"
             scores[i][j] = [1,0]
           else
             scores[i][j] = [0,1]
           end
-          j += 1
         else
           scores[i][j] = [nil, nil]
-          j += 1
         end
+        j += 1
         k += 1
       end
 
@@ -106,17 +103,18 @@ class BracketsController < ApplicationController
     return scores
   end
 
-  def gen_names(stage1, stage2, matches, players)
-    i = 0
-    j = 0
+  # Build player array for final JSON tournament bracket object
+  def generate_players(stage1, stage2, matches, players)
+    i, j, x = 0, 0, 0
     names = get_names(players)
     first_round_size = 2 ** (Math.log2(names.length).ceil - 1)
     player_names = Array.new(first_round_size, [])
     extra_players = []
+    temp_scores = []
     extra_matchups = stage1.reverse[0].compact()
-    res = PlayerMatch.select(:player_id, :match_id, :result)
+    query = PlayerMatch.select(:player_id, :match_id, :result)
     .where(match_id: extra_matchups).reorder(match_id: :asc)
-    res.each do |row|
+    query.each do |row|
       extra_players.push(get_names(row.player_id)[0])
     end
 
@@ -133,8 +131,6 @@ class BracketsController < ApplicationController
     end
 
     original_scores = stage2.reverse[0]
-    temp_scores = []
-    x = 0
     while (original_scores.length != 0)
       if ( (original_scores[x].to_s.include? "nil") )
         original_scores.shift
@@ -153,7 +149,8 @@ class BracketsController < ApplicationController
       end
       x += 1
     end
-    stage2[4] = scores
+    
+    stage2[stage2.length - 1] = scores
     bracket = {:teams => player_names, :results => stage2.reverse}
     return bracket
   end
