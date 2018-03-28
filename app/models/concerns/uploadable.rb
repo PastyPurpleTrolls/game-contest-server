13b9de70c0e2c61d84c59c8df3dce3b2a63bafb7
John Luscombe <john_luscombe@taylor.edu>
2018-03-28 11:10:09 -0400

    Consistent indentation

--- app/models/concerns/uploadable.rb
+++ app/models/concerns/uploadable.rb
@@ -2,103 +2,103 @@ require 'fileutils'
 require 'shellwords'
 
 module Uploadable
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do
-        before_destroy :delete_code_locations
-        validate :file_location_exists
+  included do
+    before_destroy :delete_code_locations
+    validate :file_location_exists
+  end
+
+  def upload=(uploaded_io)
+    unless self.name.blank?
+      self.file_location = '' if self.file_location.nil?
+      old_location = self.file_location
+      location_data = store_file(uploaded_io, self.class.to_s.downcase.pluralize)
+      self.file_location = location_data[:file]
+      dir = location_data[:directory]
+      FileUtils.mkdir_p "#{dir}/logs"
+      uncompress(self.contest.referee.compressed_file_location, dir) if self.class == Player
+      old_dir = File.dirname(old_location)
+      if File.exist?(old_dir + "/logs/")
+        cp_call = Process.spawn("cp #{old_dir}/logs/* #{dir}/logs/", :out => "/dev/null", :err => "/dev/null")
+        Process.wait cp_call
+        self.update_log_locations self.file_location
+      end
+      delete_code(old_location)
     end
+  end
 
-    def upload=(uploaded_io)
-        unless self.name.blank?
-            self.file_location = '' if self.file_location.nil?
-	    old_location = self.file_location
-            location_data = store_file(uploaded_io, self.class.to_s.downcase.pluralize)
-	    self.file_location = location_data[:file]
-	    dir = location_data[:directory]
-	    FileUtils.mkdir_p "#{dir}/logs"
-            uncompress(self.contest.referee.compressed_file_location, dir) if self.class == Player
-	    old_dir = File.dirname(old_location)
-	    if File.exist?(old_dir+"/logs/") then
-              cp_call = Process.spawn("cp #{old_dir}/logs/* #{dir}/logs/", :out=>"/dev/null", :err=>"/dev/null")
-	      Process.wait cp_call
-	      self.update_log_locations self.file_location
-	    end
-            delete_code(old_location)
-        end
+  def upload2=(uploaded_io)
+    unless self.name.blank?
+      self.compressed_file_location = '' if self.compressed_file_location.nil?
+      delete_code(self.compressed_file_location)
+      self.compressed_file_location = store_file(uploaded_io, 'environments')[:file]
     end
+  end
 
-    def upload2=(uploaded_io)
-        unless self.name.blank?
-            self.compressed_file_location = '' if self.compressed_file_location.nil?
-            delete_code(self.compressed_file_location)
-            self.compressed_file_location = store_file(uploaded_io, 'environments')[:file]
-        end
+  def upload3=(uploaded_io)
+    unless self.name.blank?
+      self.replay_assets_location = '' if self.replay_assets_location.nil?
+      delete_code(self.replay_assets_location)
+      self.replay_assets_location = store_file(uploaded_io, File.join("static", self.class.to_s.downcase.pluralize))[:directory]
     end
+  end
 
-    def upload3=(uploaded_io)
-        unless self.name.blank?
-            self.replay_assets_location = '' if self.replay_assets_location.nil?
-            delete_code(self.replay_assets_location)
-            self.replay_assets_location = store_file(uploaded_io, File.join("static", self.class.to_s.downcase.pluralize))[:directory]
-        end
+
+  #def compressed_file_location_exists
+  #   if self.compressed_file_location.nil? || !File.exists?(self.compressed_file_location)
+  #	errors.add(:compressed_file_location, "doesn't exist on the server")
+  #   end
+  #end
+  def file_location_exists
+    if self.file_location.nil? || !File.exists?(self.file_location)
+      errors.add(:file_location, "doesn't exist on the server")
     end
+  end
 
+  private
 
-    #def compressed_file_location_exists
-    #   if self.compressed_file_location.nil? || !File.exists?(self.compressed_file_location)
-    #	errors.add(:compressed_file_location, "doesn't exist on the server")
-    #   end
-    #end 
-    def file_location_exists
-        if self.file_location.nil? || !File.exists?(self.file_location)
-            errors.add(:file_location, "doesn't exist on the server")
-        end
+  def delete_code_locations
+    delete_code(self.file_location)
+    delete_code(self.compressed_file_location) if (self.has_attribute?(:compressed_file_location) && !self.compressed_file_location.nil?)
+    delete_code(self.replay_assets_location) if (self.has_attribute?(:replay_assets_location) && !self.replay_assets_location.nil?)
+  end
+
+  #Delete directory where file is located
+  def delete_code(location)
+    pathname = Pathname.new(location)
+    if not pathname.directory?
+      location = pathname.dirname
     end
-
-    private
-
-    def delete_code_locations
-        delete_code(self.file_location)
-        delete_code(self.compressed_file_location) if (self.has_attribute?(:compressed_file_location) && !self.compressed_file_location.nil?)
-        delete_code(self.replay_assets_location) if (self.has_attribute?(:replay_assets_location) && !self.replay_assets_location.nil?)
+    FileUtils.rm_rf(location) if pathname.exist?
+    if pathname.exist?
+      raise "Deleting stuff it shouldnt"
     end
+  end
 
-    #Delete directory where file is located
-    def delete_code(location)
-        pathname = Pathname.new(location)
-        if not pathname.directory?
-            location = pathname.dirname
-        end
-        FileUtils.rm_rf(location) if pathname.exist?
-        if pathname.exist? 
-            raise "Deleting stuff it shouldnt"
-        end
+  def store_file(uploaded_io, dir)
+    file_location = ''
+    dir_location = ''
+    unless uploaded_io.nil?
+      dir_location = Rails.root.join('code', dir, Rails.env, random_hex)
+      file_location = dir_location.join(uploaded_io.original_filename).to_s
+      dir_location = dir_location.to_s
+      FileUtils.mkdir_p dir_location
+      IO.copy_stream(uploaded_io, file_location)
+      uncompress(file_location, dir_location)
     end
+    {file: file_location, directory: dir_location}
+  end
 
-    def store_file(uploaded_io, dir)
-        file_location = ''
-        dir_location = ''
-        unless uploaded_io.nil?
-            dir_location = Rails.root.join('code', dir, Rails.env, random_hex)
-	    file_location = dir_location.join(uploaded_io.original_filename).to_s
-            dir_location = dir_location.to_s
-            FileUtils.mkdir_p dir_location
-            IO.copy_stream(uploaded_io, file_location)
-            uncompress(file_location, dir_location)
-        end
-        {file: file_location, directory: dir_location}
-    end
+  def uncompress(src, dest)
+    system("tar -xvf #{Shellwords.escape src} -C #{Shellwords.escape dest} > /dev/null 2>&1")
+    system("unzip -o #{Shellwords.escape src} -d #{Shellwords.escape dest} > /dev/null 2>&1")
+    system("chmod +x #{Shellwords.escape dest}/*")
+    system("dos2unix -q #{Shellwords.escape dest}/*")
+  end
 
-    def uncompress(src, dest)
-	system("tar -xvf #{Shellwords.escape src} -C #{Shellwords.escape dest} > /dev/null 2>&1")
-        system("unzip -o #{Shellwords.escape src} -d #{Shellwords.escape dest} > /dev/null 2>&1")
-        system("chmod +x #{Shellwords.escape dest}/*")
-        system("dos2unix -q #{Shellwords.escape dest}/*")
-    end
-
-    def random_hex
-	SecureRandom.hex
-    end
+  def random_hex
+    SecureRandom.hex
+  end
 end
 
